c4776830b6381f222a34d9fe14f86d49e11b6226
ianhhhhhhhhe <517382432@qq.com>
2018-07-11 14:26:53 +0800

    Update signature

--- crypto/signature.js
+++ crypto/signature.js
@@ -16,7 +16,7 @@ var ecdsa = require('secp256k1');
  * @param {string} priv_key - private key
  * @return {string} return signature result in base64
  */
-exports.sign = function(hash, priv_key){
+let sign = function(hash, priv_key){
 	var res = ecdsa.sign(hash, priv_key);
 	return res.signature.toString("base64");
 };
@@ -30,7 +30,7 @@ exports.sign = function(hash, priv_key){
  * @param {string} b64_pub_key - public key in base64
  * @return {string} return signature result
  */
-exports.verify = function(hash, b64_sig, b64_pub_key){
+let verify = function(hash, b64_sig, b64_pub_key){
 	try{
 		var signature = new Buffer(b64_sig, "base64"); // 64 bytes (32+32)
 		return ecdsa.verify(hash, signature, new Buffer(b64_pub_key, "base64"));
@@ -41,3 +41,7 @@ exports.verify = function(hash, b64_sig, b64_pub_key){
 	}
 };
 
+module.exports = { 
+	sign: sign,
+	verify: verify
+}
