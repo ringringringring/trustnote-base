63f240fa2a01d5608b721a4b70cc2b03a430adcd
ianhhhhhhhhe <517382432@qq.com>
2018-07-13 16:17:36 +0800

    Update eslint of signature

--- encrypt/signature.js
+++ encrypt/signature.js
@@ -1,12 +1,12 @@
 /* jslint node: true */
-"use strict"
+
 
 /**
  * Signature Module
  * @module signature
  */
 
-var ecdsa = require('secp256k1');
+const ecdsa = require('secp256k1')
 
 /**
  * Signature function
@@ -16,32 +16,31 @@ var ecdsa = require('secp256k1');
  * @param {string} priv_key - private key
  * @return {string} return signature result in base64
  */
-let sign = function(hash, priv_key){
-	var res = ecdsa.sign(hash, priv_key);
-	return res.signature.toString("base64");
-};
+function sign(hash, privKey) {
+    const res = ecdsa.sign(hash, privKey)
+    return res.signature.toString('base64')
+}
 
 /**
  * Validate Signature function
  * @function
  * @instance
  * @param {string} hash - hash of text to sign
- * @param {string} b64_sig - signature result in base64
- * @param {string} b64_pub_key - public key in base64
+ * @param {string} b64Sig - signature result in base64
+ * @param {string} b64PubKey - public key in base64
  * @return {string} return signature result
  */
-let verify = function(hash, b64_sig, b64_pub_key){
-	try{
-		var signature = new Buffer(b64_sig, "base64"); // 64 bytes (32+32)
-		return ecdsa.verify(hash, signature, new Buffer(b64_pub_key, "base64"));
-	}
-	catch(e){
-		console.log('signature verification exception: '+e.toString());
-		return false;
-	}
-};
+function verify(hash, b64Sig, b64PubKey) {
+    try {
+        const signature = Buffer.alloc(b64Sig, 'base64') // 64 bytes (32+32)
+        return ecdsa.verify(hash, signature, Buffer.alloc(b64PubKey, 'base64'))
+    } catch (e) {
+        console.log(`signature verification exception: ${e.toString()}`)
+        return false
+    }
+}
 
-module.exports = { 
-	sign: sign,
-	verify: verify
+module.exports = {
+    sign,
+    verify,
 }
