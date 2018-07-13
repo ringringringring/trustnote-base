bf6fee4823583db5471b7b4c24c438aa56533beb
VictorZhang <victor.zhang@thingtrust.com>
2018-07-13 17:03:40 +0800

    modi eslint

--- common/logger.js
+++ common/logger.js
@@ -1,55 +1,72 @@
-/*jslint node: true */
-"use strict";
+/* jslint node: true */
 
-var log4js = require('log4js');
+const log4js = require('log4js')
 
 log4js.configure({
-    replaceConsole: true,        // whether or not to replace console.log
+    replaceConsole: true, // whether or not to replace console.log
     appenders: {
-        out: {type: 'console'},
-        info: {type: 'dateFile', filename: './logs/infos.log', pattern: '.yyyy-MM-dd', compress: true , level: 'info'},
-        debug: {type: 'dateFile', filename: './logs/debugs.log', pattern: '.yyyy-MM-dd', compress: true , level: 'debug'},
-        warn: {type: 'dateFile', filename: './logs/warns.log', pattern: '.yyyy-MM-dd', compress: true , level: 'warn'},
-        error: {type: 'dateFile', filename: './logs/errors.log', pattern: '.yyyy-MM-dd', compress: true, level: 'error'}
+        out: { type: 'console' },
+        info: {
+            type: 'dateFile', filename: './logs/trustnote.log', pattern: '.yyyy-MM-dd', compress: true, level: 'info',
+        },
+        debug: {
+            type: 'dateFile', filename: './logs/trustnote.log', pattern: '.yyyy-MM-dd', compress: true, level: 'debug',
+        },
+        warn: {
+            type: 'dateFile', filename: './logs/trustnote.log', pattern: '.yyyy-MM-dd', compress: true, level: 'warn',
+        },
+        error: {
+            type: 'dateFile', filename: './logs/trustnote.log', pattern: '.yyyy-MM-dd', compress: true, level: 'error',
+        },
+        fatal: {
+            type: 'dateFile', filename: './logs/trustnote.log', pattern: '.yyyy-MM-dd', compress: true, level: 'fatal',
+        },
     },
     categories: {
-        default: {appenders: ['out', 'info'], level: 'info'},
-        debug: {appenders: ['out', 'debug'], level: 'debug'},
-        warn: {appenders: ['out', 'warn'], level: 'warn'},
-        error: {appenders: ['out', 'error'], level: 'error'}
-    }
-});
+        default: { appenders: ['out', 'info'], level: 'info' },
+        debug: { appenders: ['out', 'debug'], level: 'debug' },
+        warn: { appenders: ['out', 'warn'], level: 'warn' },
+        error: { appenders: ['out', 'error'], level: 'error' },
+        fatal: { appenders: ['out', 'fatal'], level: 'fatal' },
+    },
+})
 
-var levels = {
-    'trace': log4js.levels.TRACE,
-    'debug': log4js.levels.DEBUG,
-    'info': log4js.levels.INFO,
-    'warn': log4js.levels.WARN,
-    'error': log4js.levels.ERROR,
-    'fatal': log4js.levels.FATAL
-};
+// let levels = {
+//   trace: log4js.levels.TRACE,
+//   debug: log4js.levels.DEBUG,
+//   info: log4js.levels.INFO,
+//   warn: log4js.levels.WARN,
+//   error: log4js.levels.ERROR,
+//   fatal: log4js.levels.FATAL,
+// };
 
-function info(info){
-    var logger = log4js.getLogger();
-    logger.info(info);
+function info(infoMessage) {
+    const logger = log4js.getLogger()
+    logger.info(infoMessage)
 }
 
-function debug(debug){
-    var logger = log4js.getLogger("debug");
-    logger.debug(debug);
+function debug(debugMessage) {
+    const logger = log4js.getLogger('debug')
+    logger.debug(debugMessage)
 }
 
-function warn(warn){
-    var logger = log4js.getLogger("warn");
-    logger.warn(warn);
+function warn(warnMessage) {
+    const logger = log4js.getLogger('warn')
+    logger.warn(warnMessage)
 }
 
-function error(errorMessage){
-    var logger = log4js.getLogger("error");
-    logger.error(errorMessage);
+function error(errorMessage) {
+    const logger = log4js.getLogger('error')
+    logger.error(errorMessage)
 }
 
-exports.info = info;
-exports.debug = debug;
-exports.warn = warn;
-exports.error = error;
+function fatal(errorMessage) {
+    const logger = log4js.getLogger('fatal')
+    logger.fatal(errorMessage)
+}
+
+exports.info = info
+exports.debug = debug
+exports.warn = warn
+exports.error = error
+exports.fatal = fatal
