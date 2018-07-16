3d5f40d94bac0d9e92a44963647d87b692316001
ianhhhhhhhhe <517382432@qq.com>
2018-07-16 11:21:04 +0800

    Update documentation comment

--- encrypt/object_hash.js
+++ encrypt/object_hash.js
@@ -1,8 +1,20 @@
 /* jslint node: true */
 
+/**
+ * Get object chash
+ * @module object_hash
+ */
+
 const crypto = require('crypto')
 const chash = require('./chash.js')
 
+/**
+ * change everthing into string
+ * @function
+ * @private
+ * @param {Object} obj - object or others things
+ * @returns {String} string - string of obj
+ */
 function getSourceString(obj) {
     const arrComponents = []
     function extractComponents(variable) {
@@ -46,23 +58,55 @@ function getSourceString(obj) {
     return arrComponents.join('\x00')
 }
 
+/**
+ * get 160 bits chash
+ * @function
+ * @public
+ * @param {Object} obj - object to be chashed
+ * @returns {String} chash - 160 bits chashed data
+ */
 function getChash160(obj) {
     return chash.getChash160(getSourceString(obj))
 }
 
+/**
+ * get 288 bits chash
+ * @function
+ * @public
+ * @param {Object} obj - object to be chashed
+ * @returns {String} chash - 288 bits chashed data
+ */
 function getChash288(obj) {
     return chash.getChash288(getSourceString(obj))
 }
 
+/**
+ * get sha256 crypt data encode with hex
+ * @function
+ * @public
+ * @param {Object} obj - sha256 crypt object
+ * @returns {String} sha256 - 288 bits sha256 crypt data encode with hex
+ */
 function getHexHash(obj) {
     return crypto.createHash('sha256').update(getSourceString(obj), 'utf8').digest('hex')
 }
 
+/**
+ * get sha256 crypt data encode with base64
+ * @function
+ * @public
+ * @param {Object} obj - sha256 crypt object
+ * @returns {String} sha256 - 288 bits sha256 crypt data encode with base64
+ */
 function getBase64Hash(obj) {
     return crypto.createHash('sha256').update(getSourceString(obj), 'utf8').digest('base64')
 }
 
-
+/**
+ * get unit without unit, header, payload, mci, timestamp, message payload
+ * @param {Object} objUnit - a normal unit
+ * @returns {Object} nakedUnit
+ */
 function getNakedUnit(objUnit) {
     const objNakedUnit = Object.assign({}, objUnit)
     delete objNakedUnit.unit
@@ -82,10 +126,20 @@ function getNakedUnit(objUnit) {
     return objNakedUnit
 }
 
+/**
+ * get unit's content sha256 and encode it with base64
+ * @param {Object} objUnit - unit
+ * @returns {String} unit hash
+ */
 function getUnitContentHash(objUnit) {
     return getBase64Hash(getNakedUnit(objUnit))
 }
 
+/**
+ * get unit hash encode in base64
+ * @param {Object} objUnit - unit
+ * @returns {String} hash - unit hash
+ */
 function getUnitHash(objUnit) {
     if (objUnit.content_hash) { // already stripped
         return getBase64Hash(getNakedUnit(objUnit))
@@ -106,6 +160,11 @@ function getUnitHash(objUnit) {
     return getBase64Hash(objStrippedUnit)
 }
 
+/**
+ * get unit hash to sign
+ * @param {unit} objUnit - unit to be signed
+ * @returns {String} hash
+ */
 function getUnitHashToSign(objUnit) {
     const objNakedUnit = getNakedUnit(objUnit)
     for (let i = 0; i < objNakedUnit.authors.length; i += 1) {
@@ -114,6 +173,14 @@ function getUnitHashToSign(objUnit) {
     return crypto.createHash('sha256').update(getSourceString(objNakedUnit), 'utf8').digest()
 }
 
+/**
+ * get ball hash
+ * @param {Object} unit - unit
+ * @param {Array} arrParentBalls - array of parents' ball
+ * @param {Array} arrSkiplistBalls - skipball list
+ * @param {Boolean} bNonserial - if unit is serial
+ * @returns {String} ball hash
+ */
 function getBallHash(unit, arrParentBalls, arrSkiplistBalls, bNonserial) {
     const objBall = {
         unit,
@@ -124,11 +191,21 @@ function getBallHash(unit, arrParentBalls, arrSkiplistBalls, bNonserial) {
     return getBase64Hash(objBall)
 }
 
+/**
+ * get joint hash
+ * @param {Object} objJoint - joint
+ * @returns {String} joint hash
+ */
 function getJointHash(objJoint) {
     // we use JSON.stringify, we can't use objectHash here because it might throw errors
     return crypto.createHash('sha256').update(JSON.stringify(objJoint), 'utf8').digest('base64')
 }
 
+/**
+ * delete all null proto
+ * @param {Object} obj - object
+ * @returns {Object} clean object
+ */
 function cleanNulls(obj) {
     Object.keys(obj).forEach((key) => {
         if (obj[key] === null) {
@@ -146,10 +223,21 @@ function cleanNulls(obj) {
 // a payment address that the device owner knows a private key for,
 // because payment address is derived by c-hashing the definition object, while device
 //  address is produced from raw public key.
+
+/**
+ * get device address
+ * @param {String} b64Pubkey - pubkey in base64
+ * @returns {String} device address
+ */
 function getDeviceAddress(b64Pubkey) {
     return (`0${getChash160(b64Pubkey)}`)
 }
 
+/**
+ * get device message
+ * @param {Object} objDeviceMessage - messages
+ * @return {String} message ahash
+ */
 function getDeviceMessageHashToSign(objDeviceMessage) {
     const objNakedDeviceMessage = Object.assign({}, objDeviceMessage)
     delete objNakedDeviceMessage.signature
