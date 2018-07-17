9199deaed40a0f15b9c467202f465ff79c831be0
VictorZhang <victor.zhang@thingtrust.com>
2018-07-17 09:34:07 +0800

    modi logger

--- common/logger.js
+++ common/logger.js
@@ -1,9 +1,9 @@
 /* jslint node: true */
-'use strict'
 
-const log4js = require('log4js');
-log4js.configure('./config/log.json');
-const logger = log4js.getLogger('trustnote');
+const log4js = require('log4js')
+
+log4js.configure('./config/log.json')
+const logger = log4js.getLogger('trustnote')
 
 /**
 * when running in a production environment, we don't print the debug information
