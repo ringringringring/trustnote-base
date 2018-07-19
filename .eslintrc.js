4a17a8eb84e183db33b9f2326869f7dc1ed53ab1
VictorZhang <victor.zhang@thingtrust.com>
2018-07-19 13:59:08 +0800

    modi eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,20 @@ module.exports = {
     },
     "extends": "airbnb-base",
     "parserOptions": {
-        "ecmaVersion": 2015,
+        "ecmaVersion": 2017,
         "sourceType": "module"
     },
     "rules": {
         "indent": ["warn", 4],
         "linebreak-style": ["error", "unix"],
         "quotes": ["error", "single"],
+        "max-len": ["error", { "code": 200 }],
         "semi": ["warn", "never"],
         "no-param-reassign": ["off", "unix"],
         "import/no-dynamic-require": 0,
         "global-require": 0,
         "no-use-before-define": ["error", { "functions": false, "classes": false }],
-        "no-plusplus": ["error", {"allowForLoopAfterthoughts": true}]
+        "no-plusplus": ["error", {"allowForLoopAfterthoughts": true}],
+        "consistent-return": 0
     }
 };
