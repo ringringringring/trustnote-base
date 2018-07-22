a6b96977747b93d1e17164ae862edf4409cb057f
ianhhhhhhhhe <517382432@qq.com>
2018-07-22 23:45:21 +0800

    Update some call back with await method, and update some database process

--- sharedAddress/composer.js
+++ sharedAddress/composer.js
@@ -34,7 +34,9 @@ function handleNewSharedAddress(body) {
         if (typeof body.signers !== 'object' || Object.keys(body.signers).length === 0) return reject(Error('invalid signers'))
         if (body.address !== objectHash.getChash160(body.definition)) return reject(Error('definition doesn\'t match its c-hash'))
         Object.values(body.signers).forEach((signerInfo) => {
-            if (signerInfo.address && !validationUtils.isValidAddress(signerInfo.address)) return reject(Error(`invalid member address: ${signerInfo.address}`))
+            if (signerInfo.address && !validationUtils.isValidAddress(signerInfo.address)) {
+                return reject(Error(`invalid member address: ${signerInfo.address}`))
+            }
             return undefined
         })
         return resolve()
@@ -42,41 +44,37 @@ function handleNewSharedAddress(body) {
 }
 
 
-function determineIfIncludesMe(assocSignersByPath, handleResult) {
-    return new Promise((resolve, reject) => {
+function determineIfIncludesMe(assocSignersByPath) {
+    return new Promise(async (resolve, reject) => {
         const assocMemberAddresses = {}
         Object.values(assocSignersByPath).forEach((signerInfo) => {
             if (signerInfo.address) assocMemberAddresses[signerInfo.address] = true
         })
         const arrMemberAddresses = Object.keys(assocMemberAddresses)
         if (arrMemberAddresses.length === 0) return reject(Error('no member addresses?'))
-        db.query(
-            'SELECT address FROM my_addresses WHERE address IN(?) UNION SELECT shared_address AS address FROM shared_addresses WHERE shared_address IN(?)',
-            [arrMemberAddresses, arrMemberAddresses],
-            (rows) => {
-                handleResult(rows.length > 0 ? null : 'I am not a member of this shared address')
-            },
-        )
+        const rows = await db.selectSharedAddressUnionMyAddress(arrMemberAddresses)
+        if (rows.length <= 0) {
+            reject(Error('I am not a member of this shared address'))
+        }
         return resolve()
     })
 }
 
 function validateAddressDefinition(arrDefinition) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         const objFakeUnit = { authors: [] }
         const objFakeValidationState = {
             last_ball_mci: MAX_INT32,
             bAllowUnresolvedInnerDefinitions: true,
         }
-        definition.validateDefinition(db, arrDefinition, objFakeUnit,
-            objFakeValidationState, false, (err) => {
-                if (err) return reject(Error(err))
-                return resolve()
-            })
+        const res = await definition.validateDefinition(db, arrDefinition, objFakeUnit,
+            objFakeValidationState, false)
+        if (res) return reject(Error(res))
+        return resolve()
     })
 }
 
-function forwardNewSharedAddressToCosignersOfMyMemberAddresses(address,
+async function forwardNewSharedAddressToCosignersOfMyMemberAddresses(address,
     arrDefinition, assocSignersByPath) {
     const assocMyMemberAddresses = {}
     Object.values(assocSignersByPath).forEach((signerInfo) => {
@@ -86,36 +84,24 @@ function forwardNewSharedAddressToCosignersOfMyMemberAddresses(address,
     })
     const arrMyMemberAddresses = Object.keys(assocMyMemberAddresses)
     if (arrMyMemberAddresses.length === 0) throw Error('my member addresses not found')
-    db.query(
-        'SELECT DISTINCT device_address FROM my_addresses JOIN wallet_signing_paths USING(wallet) WHERE address IN(?) AND device_address!=?',
-        [arrMyMemberAddresses, device.getMyDeviceAddress()],
-        (rows) => {
-            rows.forEach((row) => {
-                sendNewSharedAddress(row.device_address, address,
-                    arrDefinition, assocSignersByPath, true)
-            })
-        },
-    )
+    const rows = await db.selectSharedAddressPartner(arrMyMemberAddresses,
+        device.getMyDeviceAddress)
+    rows.forEach((row) => {
+        sendNewSharedAddress(row.device_address, address,
+            arrDefinition, assocSignersByPath, true)
+    })
 }
 
 function addNewSharedAddress(address, arrDefinition, assocSignersByPath) {
     return new Promise((resolve) => {
-        db.query(
-            `INSERT ${db.getIgnore()} INTO shared_addresses (shared_address, definition) VALUES (?,?)`,
-            [address, JSON.stringify(arrDefinition)],
-            () => {
-                const arrQueries = []
-                Object.keys(assocSignersByPath).forEach((signingPath) => {
-                    const signerInfo = assocSignersByPath[signingPath]
-                    db.addQuery(arrQueries,
-                        `INSERT ${db.getIgnore()} INTO shared_address_signing_paths
-                            (shared_address, address, signing_path, member_signing_path, device_address) VALUES (?,?,?,?,?)`,
-                        [address, signerInfo.address, signingPath,
-                            signerInfo.member_signing_path, signerInfo.device_address])
-                })
-                resolve(arrQueries)
-            },
-        )
+        db.insertSharedAddreess(address, JSON.stringify(arrDefinition))
+        const arrQueries = []
+        Object.keys(assocSignersByPath).forEach((signingPath) => {
+            const signerInfo = assocSignersByPath[signingPath]
+            db.insertSharedAddreessSigningPaths(address, signerInfo.address, signingPath,
+                signerInfo.member_signing_path, signerInfo.device_address)
+        })
+        resolve(arrQueries)
     })
 }
 
@@ -134,7 +120,7 @@ async function createNewSharedAddress(arrDefinition, assocSignersByPath) {
     const arrQueries = await addNewSharedAddress(body.address, body.definition,
         body.signers, body.forward)
     arrQueries.forEach(() => {
-        console.log(`added new shared address ${address}`)
+        // console.log(`added new shared address ${address}`)
         eventBus.emit(`new_address-${address}`)
         if (conf.bLight) {
             network.addLightWatchedAddress(address)
