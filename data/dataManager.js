93b49ee8817c20dee4c43e0e654bf0cd4cd91102
Dream <wwuwwei@126.com>
2018-07-25 10:36:30 +0800

    fix eslint error

--- data/dataManager.js
+++ data/dataManager.js
@@ -1,16 +1,16 @@
 
-const dag = require('./dag/dag')
+const dag = require('./dag/dag');
 
 async function getParentsFromFreeUnits() {
     const dagInstance = await dag.getInstance();
-    // combine good sequence unit and exclude archived joints 
-    const parents= dagInstance.tipUnitsWithGoodSequence()
-    const archived= dagInstance.archivedJoints()
-    if (archived && archived.length >0){
-        parents = parents.filter ( (unit)=> return archived.index(unit) == -1 );
+    // combine good sequence unit and exclude archived joints
+    let parents = dagInstance.tipUnitsWithGoodSequence();
+    const archived = dagInstance.archivedJoints();
+    if (archived && archived.length > 0) {
+        parents = parents.filter(unit => archived.index(unit) === -1);
     }
-    
-    return parents
+
+    return parents;
 }
 
 
