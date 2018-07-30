a90e50f590d9c9e806b1ad8cda2337ace351d807
ianhhhhhhhhe <517382432@qq.com>
2018-07-30 09:28:13 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "airbnb-base",
+    "extends": "eslint-config-airbnb",
     "parserOptions": {
         "ecmaVersion": 2017,
         "sourceType": "module"
